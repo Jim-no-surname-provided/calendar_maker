6028cb9a1768a9de0bdaaeb2f951ce744bbe2810
Jim <helmutjpfeffer@gmail.com>
2026-05-28 12:32:57 +0200

    Added all buttons

--- src/gui.py
+++ src/gui.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 import customtkinter as ctk
 from PIL import Image, ImageTk
 
-from model import CalendarModel, DaySchedule, Platform, WeekDay
+from model import CalendarModel, CollabMember, DaySchedule, Platform, WeekDay
 from renderer import RenderResources, render_calendar
 
 
@@ -23,8 +23,11 @@ class App:
         self.day_widgets = {}
         self.render_resources = RenderResources()
         self.day_preview_ctk_images = {}
+        self.day_collab_rows = {}
         self.preview_canvas_image = None
         self.rendered_preview_image = None
+        self.preview_update_job = None
+        self.platform_ctk_images = {}
 
         self.create_layout()
 
@@ -130,6 +133,38 @@ class App:
         for day in WeekDay:
             self.make_day_section(day)
 
+    def labeled_row(
+        self,
+        parent,
+        label_text: str,
+        variable: ctk.StringVar,
+        placeholder_text: str,
+        padx=12,
+        pady=(0, 12),
+    ):
+        # Make row
+        row = ctk.CTkFrame(parent, fg_color="transparent")
+        row.pack(fill="x", padx=padx, pady=pady)
+
+        # Make label
+        label = ctk.CTkLabel(
+            row,
+            text=label_text,
+            width=80,
+            anchor="w",
+        )
+        label.pack(side="left")
+
+        # Make entry
+        entry = ctk.CTkEntry(
+            row,
+            textvariable=variable,
+            placeholder_text=placeholder_text,
+        )
+        entry.pack(side="left", fill="x", expand=True, padx=(8, 0))
+
+        return row, label, entry
+
     def make_header_section(self):
         # Make header section frame
         self.header_section = ctk.CTkFrame(self.left_content)
@@ -140,25 +175,13 @@ class App:
         self.date_range_var.trace_add("write", self.on_date_range_changed)
 
         # Make date range row
-        self.date_range_row = ctk.CTkFrame(self.header_section, fg_color="transparent")
-        self.date_range_row.pack(fill="x", padx=12, pady=(12, 6))
-
-        # Make date range label
-        self.date_range_label = ctk.CTkLabel(
-            self.date_range_row,
-            text="Fecha",
-            width=80,
-            anchor="w",
+        self.date_range_row, self.date_range_label, self.date_range_entry = self.labeled_row(
+            self.header_section,
+            "Fecha",
+            self.date_range_var,
+            "Ej. 12 mayo - 18 mayo",
+            pady=(12, 6),
         )
-        self.date_range_label.pack(side="left")
-
-        # Make date range entry
-        self.date_range_entry = ctk.CTkEntry(
-            self.date_range_row,
-            textvariable=self.date_range_var,
-            placeholder_text="Ej. 12 mayo - 18 mayo",
-        )
-        self.date_range_entry.pack(side="left", fill="x", expand=True, padx=(8, 0))
 
         # Make fanart label
         self.fanart_label = ctk.CTkLabel(
@@ -280,19 +303,19 @@ class App:
         self.make_rest_day_checkbox(day_section, day_schedule, day_widgets)
 
         # Make day image selector
-        self.make_day_image_selector(day_section, day_schedule)
+        self.make_day_image_selector(day_section, day_schedule, day_widgets)
 
         # Make stream title row
         self.make_stream_title_row(day_section, day_schedule, day_widgets)
 
         # Make collab row
-        self.make_collab_row(day_section, day_schedule)
+        self.make_collab_row(day_section, day_schedule, day_widgets)
 
         # Make time row
-        self.make_time_row(day_section, day_schedule)
+        self.make_time_row(day_section, day_schedule, day_widgets)
 
         # Make platform selector
-        self.make_platform_selector(day_section, day_schedule)
+        self.make_platform_selector(day_section, day_schedule, day_widgets)
 
     def make_day_title(self, parent, day_schedule: DaySchedule):
         # Make day title label
@@ -324,7 +347,7 @@ class App:
         )
         rest_day_checkbox.pack(anchor="w", padx=12, pady=(0, 12))
 
-    def make_day_image_selector(self, parent, day_schedule: DaySchedule):
+    def make_day_image_selector(self, parent, day_schedule: DaySchedule, day_widgets):
 
         # Make image selector frame
         image_selector = ctk.CTkFrame(parent, fg_color="transparent")
@@ -335,6 +358,7 @@ class App:
             image_selector,
             height=140,
             corner_radius=8,
+            fg_color=self.fanart_preview.cget("fg_color"),
         )
         image_preview.pack(fill="x", pady=(0, 8))
         image_preview.pack_propagate(False)
@@ -355,6 +379,8 @@ class App:
         )
         select_button.pack(fill="x")
 
+        day_widgets.append(image_selector)
+
         if day_schedule.image_path is not None:
             self.update_day_image_preview(day_schedule.day, day_schedule.image_path)
 
@@ -367,37 +393,229 @@ class App:
         )
 
         # Make stream title row
-        stream_title_row = ctk.CTkFrame(parent, fg_color="transparent")
-        stream_title_row.pack(fill="x", padx=12, pady=(0, 12))
-
-        # Make stream title label
-        stream_title_label = ctk.CTkLabel(
-            stream_title_row,
-            text="Título",
-            width=80,
-            anchor="w",
+        stream_title_row, _, _ = self.labeled_row(
+            parent,
+            "Título",
+            stream_title_var,
+            "Título del stream",
         )
-        stream_title_label.pack(side="left")
-
-        # Make stream title entry
-        stream_title_entry = ctk.CTkEntry(
-            stream_title_row,
-            textvariable=stream_title_var,
-            placeholder_text="Título del stream",
-        )
-        stream_title_entry.pack(side="left", fill="x", expand=True, padx=(8, 0))
 
         day_widgets.append(stream_title_row)
-        day_widgets.append(stream_title_entry)
 
-    def make_collab_row(self, parent, day_schedule: DaySchedule):
+    def make_collab_row(self, parent, day_schedule: DaySchedule, day_widgets):
+        # Make collab section frame
+        collab_section = ctk.CTkFrame(parent, fg_color="transparent")
+        collab_section.pack(fill="x", padx=12, pady=(0, 12))
+        day_widgets.append(collab_section)
+
+        # Make collab header row
+        collab_header_row = ctk.CTkFrame(collab_section, fg_color="transparent")
+        collab_header_row.pack(fill="x", pady=(0, 8))
+
+        # Make collab label
+        collab_label = ctk.CTkLabel(
+            collab_header_row,
+            text="Collab",
+            anchor="w",
+        )
+        collab_label.pack(side="left", fill="x", expand=True)
+
+        # Make add collab button
+        add_collab_button = ctk.CTkButton(
+            collab_header_row,
+            text="+",
+            width=36,
+            command=lambda: self.add_collab_member_row(day_schedule, collab_list),
+        )
+        add_collab_button.pack(side="right")
+
+        # Make collab list frame
+        collab_list = ctk.CTkFrame(
+            collab_section,
+            fg_color="transparent",
+            height=0,
+        )
+        collab_list.pack(fill="x")
+        collab_list.pack_propagate(False)
+        self.day_collab_rows[day_schedule.day] = []
+
+        # Add existing collab members
+        for collab_member in day_schedule.collab_members:
+            self.add_collab_member_row(day_schedule, collab_list, collab_member)
+
+    def add_collab_member_row(
+        self,
+        day_schedule: DaySchedule,
+        parent,
+        collab_member: CollabMember | None = None,
+    ):
+        if collab_member is None:
+            collab_member = CollabMember()
+
+        parent.pack_propagate(True)
+
+        # Make collab member variable
+        name_var = ctk.StringVar(value=collab_member.name)
+
+        # Make collab member row
+        collab_member_row = ctk.CTkFrame(parent, fg_color="transparent")
+        collab_member_row.pack(fill="x", pady=(0, 6))
+
+        # Make collab member entry
+        collab_member_entry = ctk.CTkEntry(
+            collab_member_row,
+            textvariable=name_var,
+            placeholder_text="Nombre",
+        )
+        collab_member_entry.pack(side="left", fill="x", expand=True)
+
+        # Make collab color button
+        color_button = ctk.CTkButton(
+            collab_member_row,
+            text="",
+            width=32,
+            fg_color=collab_member.color,
+            hover_color=collab_member.color,
+            command=lambda: self.on_select_collab_color(color_button, collab_member),
+        )
+        color_button.pack(side="left", padx=(8, 8))
+
+        # Make remove collab button
+        remove_button = ctk.CTkButton(
+            collab_member_row,
+            text="-",
+            width=36,
+            command=lambda: self.remove_collab_member_row(day_schedule, collab_member_row),
+        )
+        remove_button.pack(side="left")
+
+        name_var.trace_add(
+            "write",
+            lambda *_: self.on_collab_changed(day_schedule),
+        )
+
+        self.day_collab_rows[day_schedule.day].append(
+            {
+                "row": collab_member_row,
+                "name_var": name_var,
+                "color": collab_member.color,
+            }
+        )
+
+        self.on_collab_changed(day_schedule)
+
+    def remove_collab_member_row(self, day_schedule: DaySchedule, collab_member_row):
+        rows = self.day_collab_rows[day_schedule.day]
+        self.day_collab_rows[day_schedule.day] = [
+            row_data for row_data in rows if row_data["row"] != collab_member_row
+        ]
+
+        collab_member_row.destroy()
+
+        if not self.day_collab_rows[day_schedule.day]:
+            collab_member_row.master.configure(height=0)
+            collab_member_row.master.pack_propagate(False)
+
+        self.on_collab_changed(day_schedule)
+
+    def on_select_collab_color(self, color_button, collab_member: CollabMember):
+        # TODO: Open color picker and update collab color
         pass
 
-    def make_time_row(self, parent, day_schedule: DaySchedule):
-        pass
+    def make_time_row(self, parent, day_schedule: DaySchedule, day_widgets):
+        # Make time variable
+        time_var = ctk.StringVar(value=day_schedule.time_text)
+        time_var.trace_add(
+            "write",
+            lambda *_: self.on_time_changed(day_schedule, time_var),
+        )
 
-    def make_platform_selector(self, parent, day_schedule: DaySchedule):
-        pass
+        # Make time row
+        time_row, _, _ = self.labeled_row(
+            parent,
+            "Hora",
+            time_var,
+            "Ej. 19:00 CET",
+        )
+
+        day_widgets.append(time_row)
+
+    def make_platform_selector(self, parent, day_schedule: DaySchedule, day_widgets):
+        # Make platform variable
+        platform_var = ctk.StringVar(value=day_schedule.platform.value)
+
+        # Make platform row
+        platform_row = ctk.CTkFrame(parent, fg_color="transparent")
+        platform_row.pack(fill="x", padx=12, pady=(0, 12))
+
+        # Make centered container
+        platform_options = ctk.CTkFrame(
+            platform_row,
+            fg_color="transparent",
+        )
+        platform_options.pack()
+
+        def make_platform_option(platform: Platform):
+            # Make platform option frame
+            option_frame = ctk.CTkFrame(platform_options, fg_color="transparent")
+
+            # Make platform radio button
+            radio_button = ctk.CTkRadioButton(
+                option_frame,
+                text="",
+                width=20,
+                radiobutton_width=16,
+                radiobutton_height=16,
+                variable=platform_var,
+                value=platform.value,
+                command=lambda: self.on_platform_changed(day_schedule, platform_var),
+            )
+            radio_button.pack(side="left")
+
+            # Load platform icon
+            icon_image = self.render_resources.load_platform_icon(
+                platform,
+                28,
+                28,
+            )
+
+            # Convert platform icon to CTkImage
+            icon = ctk.CTkImage(
+                light_image=icon_image,
+                dark_image=icon_image,
+                size=(28, 28),
+            )
+            self.platform_ctk_images[(day_schedule.day, platform)] = icon
+
+            # Make platform icon label
+            icon_label = ctk.CTkLabel(
+                option_frame,
+                text="",
+                image=icon,
+            )
+            icon_label.pack(side="left", padx=(4, 0))
+
+            return option_frame
+
+        # Make Twitch option
+        twitch_option = make_platform_option(Platform.TWITCH)
+        twitch_option.pack(side="left")
+
+        # Make fixed spacing
+        platform_spacing = ctk.CTkFrame(
+            platform_options,
+            width=16,
+            height=1,
+            fg_color="transparent",
+        )
+        platform_spacing.pack(side="left")
+        platform_spacing.pack_propagate(False)
+
+        # Make YouTube option
+        youtube_option = make_platform_option(Platform.YOUTUBE)
+        youtube_option.pack(side="left")
+
+        day_widgets.append(platform_row)
 
     def on_rest_day_changed(self, day_schedule: DaySchedule, rest_day_var, day_widgets):
         day_schedule.is_rest_day = rest_day_var.get()
@@ -462,16 +680,47 @@ class App:
         day_schedule.title = stream_title_var.get()
         self.on_model_changed()
 
-    def on_collab_changed(self, day: WeekDay):
-        pass
+    def on_collab_changed(self, day_schedule: DaySchedule):
+        collab_members = []
 
-    def on_time_changed(self, day: WeekDay):
-        pass
+        for row_data in self.day_collab_rows[day_schedule.day]:
+            name = row_data["name_var"].get().strip()
 
-    def on_platform_changed(self, day: WeekDay):
-        pass
+            if not name:
+                continue
+
+            collab_members.append(
+                CollabMember(
+                    name=name,
+                    color=row_data["color"],
+                )
+            )
+
+        day_schedule.collab_members = collab_members
+        self.on_model_changed()
+
+    def on_time_changed(self, day_schedule: DaySchedule, time_var):
+        day_schedule.time_text = time_var.get()
+        self.on_model_changed()
+
+    def on_platform_changed(self, day_schedule: DaySchedule, platform_var):
+        day_schedule.platform = Platform(platform_var.get())
+        self.on_model_changed()
 
     def on_model_changed(self):
+        self.schedule_preview_update()
+
+    def schedule_preview_update(self):
+        if self.preview_update_job is not None:
+            self.root.after_cancel(self.preview_update_job)
+
+        self.preview_update_job = self.root.after(
+            120,
+            self.render_preview,
+        )
+
+    def render_preview(self):
+        self.preview_update_job = None
         self.rendered_preview_image = render_calendar(self.model, self.render_resources)
         self.update_preview_canvas()
 
--- src/renderer.py
+++ src/renderer.py
@@ -1,22 +1,76 @@
 from dataclasses import dataclass, field
+from io import BytesIO
+from pathlib import Path
+
+import cairosvg
 from PIL import Image
-from model import CalendarModel
+
+from model import CalendarModel, Platform
 
 
-@dataclass
-class RenderResources:
-    images: dict[str, Image.Image] = field(default_factory=dict)
-
-    def load_image(self, path: str) -> Image.Image:
-        if path not in self.images:
-            self.images[path] = Image.open(path).convert("RGBA")
-
-        return self.images[path].copy()
-
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+RESOURCE_DIR = PROJECT_ROOT / "resources"
 
 CANVAS_WIDTH = 1080
 CANVAS_HEIGHT = 1920
 
 
+@dataclass
+class RenderResources:
+    images: dict[Path, Image.Image] = field(default_factory=dict)
+    svgs: dict[tuple[Path, int, int], Image.Image] = field(default_factory=dict)
+
+    def load_image(self, path: str | Path) -> Image.Image:
+        path = self.resolve_path(path)
+
+        if path not in self.images:
+            self.images[path] = Image.open(path).convert("RGBA")
+
+        return self.images[path].copy()
+
+    def load_svg(self, path: str | Path, width: int, height: int) -> Image.Image:
+        path = self.resolve_path(path)
+        cache_key = (path, width, height)
+
+        if cache_key not in self.svgs:
+            png_bytes = cairosvg.svg2png(
+                url=str(path),
+                output_width=width,
+                output_height=height,
+            )
+            if png_bytes is None:
+                raise RuntimeError(f"Could not render SVG: {path}")
+
+            self.svgs[cache_key] = Image.open(BytesIO(png_bytes)).convert("RGBA")
+
+        return self.svgs[cache_key].copy()
+
+    def load_platform_icon(
+        self,
+        platform: Platform,
+        width: int,
+        height: int,
+    ) -> Image.Image:
+        filename_by_platform = {
+            Platform.TWITCH: "TW.svg",
+            Platform.YOUTUBE: "YT.svg",
+        }
+
+        return self.load_svg(
+            RESOURCE_DIR / "platform" / filename_by_platform[platform],
+            width,
+            height,
+        )
+
+    def resolve_path(self, path: str | Path) -> Path:
+        path = Path(path)
+
+        if path.is_absolute():
+            return path
+
+        return PROJECT_ROOT / path
+
+
 def render_calendar(model: CalendarModel, resources: RenderResources) -> Image.Image:
-    return resources.load_image(r"resources\FONDO\BG.PNG")
+    # TODO: Make this function
+    return resources.load_image(RESOURCE_DIR / "FONDO" / "BG.PNG")
